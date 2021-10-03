8e78aa0d55d5aa2517cfbb3e3e92d01925564d2f
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-10-03 19:47:45 +0900

    コマをEnterで進めるコードの実装

--- position.py
+++ position.py
@@ -1,7 +1,10 @@
-pl_pos = 6
-com_pos = 3
+pl_pos = 1
+com_pos = 1
 def banmen():
     print("・"*(pl_pos-1) + "P" + "・"*(30-pl_pos) + "Goal")
     print("・"*(com_pos-1) + "C" + "・"*(30-com_pos) + "Goal")
-
-banmen()
+while True:
+    banmen()
+    input("Enterを押すとコマが進む")
+    pl_pos = pl_pos +1
+    com_pos = com_pos + 2
