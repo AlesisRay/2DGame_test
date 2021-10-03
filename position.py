3f843dc2772e55ac5f82ba78c7e1936dfe140d16
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-10-03 20:15:54 +0900

    ゴール判定の実装

--- position.py
+++ position.py
@@ -4,8 +4,28 @@ com_pos = 1
 def banmen():
     print("・"*(pl_pos-1) + "P" + "・"*(30-pl_pos) + "Goal")
     print("・"*(com_pos-1) + "C" + "・"*(30-com_pos) + "Goal")
+banmen()
+print("Start")
 while True:
+# あなたのコマ
+    input("Enterを押すとあなたのコマが進みます")
+    rp = random.randint(1,6)
+    pl_pos = pl_pos + rp
+    print("あなたのサイコロの目は"+str(rp))
+    if pl_pos > 30:
+        pl_pos = 30
     banmen()
-    input("Enterを押すとコマが進む")
-    pl_pos = pl_pos + random.randint(1,6)
-    com_pos = com_pos + random.randint(1,6)
+    if pl_pos == 30:
+        print("あなたの勝ちです")
+        break
+# コンピュータのコマ
+    input("Enterを押すとコンピュータのコマが進みます")
+    rc = random.randint(1,6)
+    com_pos = com_pos + rc
+    print("コンピュータのサイコロの目は"+str(rc))
+    if com_pos > 30:
+        com_pos = 30
+    banmen()
+    if com_pos == 30:
+        print("コンピュータの勝ちです")
+        break
